ef6c687e9871fd08ba9564fd2f2842601b3b7d0d
alexandergosta <alexander.gosta@gmail.com>
2023-11-17 11:18:30 +0100

    fixed errors

--- functions.py
+++ functions.py
@@ -2,17 +2,15 @@ def add(a, b):
     return a + b
 
 def subtract(a, b):
-    return a + b  # <--- fix this in step 7
+    return a - b  # <--- fix this in step 7
 
 def multiply(a, b):
     return a * b
 
 def convert_fahrenheit_to_celsius(fahrenheit):
-    try:
-    fahrenheit > -600
-    except AssertionError:
-    print("below absolute zero")
-    return  multiply(subtract(fahrenheit, 32), 5 / 9) # <-- Fix this in step 
+    value = multiply(subtract(fahrenheit, 32), 5 / 9) # <-- Fix this in step 
+    assert value > -273
+    return value
 
     
 
